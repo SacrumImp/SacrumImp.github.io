eea048ca27905c59ea677e4b99e72a56858c91b3
Vladislav Alpeev <vlad.alpeev@mail.ru>
2020-05-27 18:32:39 +0300

    build

---
+++ dz/build/precache-manifest.5d4046f1d14a12f477278b0c6c9f0c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aec7607a2f7359d5013c925cccefeb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba390e8a7989325049d7",
+    "url": "/static/css/main.28f6a5cb.chunk.css"
+  },
+  {
+    "revision": "c53229a7ea9ad654ab43",
+    "url": "/static/js/2.c5ed7e01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5ed7e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba390e8a7989325049d7",
+    "url": "/static/js/main.360afd9c.chunk.js"
+  },
+  {
+    "revision": "a9e92ad10f72dcef6429",
+    "url": "/static/js/runtime-main.f50bb790.js"
+  },
+  {
+    "revision": "b912289417445b10a1314cc8d99ff739",
+    "url": "/static/media/logo.b9122894.svg"
+  }
+]);
